ba992840512c11b1666360334af7fc7b082d8a32
pedro perone <pperone@gmail.com>
2023-04-24 15:06:56 -0700

    Fixing js files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,4 @@
-import "./controllers";import "controllers"
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import * as bootstrap from "bootstrap"
